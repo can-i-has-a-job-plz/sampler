67ec0e3d86b0272e5fc2f576d285d290522925cd
ojab <ojab@ojab.ru>
2016-10-26 06:47:05 +0000

    Make RuboCop happy

--- spec/sampler_spec.rb
+++ spec/sampler_spec.rb
@@ -1,7 +1,8 @@
-require "spec_helper"
+# frozen_string_literal: true
+require 'spec_helper'
 
 describe Sampler do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Sampler::VERSION).not_to be nil
   end
 end
