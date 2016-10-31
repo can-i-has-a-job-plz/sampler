7b0e4797b1e668c16150560396873314967dad03
ojab <ojab@ojab.ru>
2016-10-31 16:18:23 +0000

    Add FilterSet#match

--- lib/sampler/filter_set.rb
+++ lib/sampler/filter_set.rb
@@ -15,6 +15,18 @@ module Sampler
       raise FilterClassNotSupported, 'Unsupported filter class'
     end
 
+    def match(event)
+      each do |filter|
+        return true if case filter
+                       when String then event.url.include?(filter)
+                       when Regexp then event.url.match(filter)
+                       when Proc then filter.call(event)
+                         # TODO: should we do something in else case?
+                       end
+      end
+      false
+    end
+
     private
 
     def supported?(filter)
