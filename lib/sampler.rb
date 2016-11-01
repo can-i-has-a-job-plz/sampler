51e68912b8bd4378fcaa70cfe13c4ec13ca4f751
ojab <ojab@ojab.ru>
2016-11-01 12:14:10 +0000

    Add barebone Configuration

--- lib/sampler.rb
+++ lib/sampler.rb
@@ -2,6 +2,15 @@
 require 'sampler/version'
 require 'sampler/railtie' if defined?(Rails)
 
-module Sampler # :nodoc:
-  # Your code goes here...
+# Just a namespace for all Sampler code
+module Sampler
+  autoload :Configuration, 'sampler/configuration'
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ require 'rails'
 require 'sampler'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.before { Sampler.instance_variable_set(:@configuration, nil) }
+end
