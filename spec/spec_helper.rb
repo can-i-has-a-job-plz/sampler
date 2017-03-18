c66abe445133fe39621728db2d9e32627896c189
ojab <ojab@ojab.ru>
2017-03-18 19:11:10 +0000

    Add shoulda-matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pry-byebug'
 require 'rails'
 require 'sampler'
+require 'shoulda-matchers'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.alias_it_should_behave_like_to :include_context
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+  end
 end
