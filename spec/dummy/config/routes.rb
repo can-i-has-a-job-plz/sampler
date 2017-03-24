af7c83aae3f691a4431c5b57937b637e221e9cf7
ojab <ojab@ojab.ru>
2017-03-24 09:38:49 +0000

    Move routes from rails helper to dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   mount Sampler::Engine => "/sampler"
+
+  mount Rails.application => '/loop', internal: true
+
+  resources :authors
+
+  get '/books/:id', id: /\d+/, to: 'controller#action'
+  get '/books/*whatever', to: 'controller#action'
 end
