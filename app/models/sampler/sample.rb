d7c66aa66a17ddbf48f7c3b58eb05e2ae97509f3
ojab <ojab@ojab.ru>
2017-03-27 11:29:11 +0000

    Add tag filtering

--- app/models/sampler/sample.rb
+++ app/models/sampler/sample.rb
@@ -5,5 +5,10 @@ module Sampler
     validates :endpoint, :url, :request_method, presence: true
     validates :params, :request_body, :response_body, :tags,
               exclusion: { in: [nil], message: 'cannot be nil' }
+
+    def self.with_tags(tags)
+      return where("tags = '{}'") if tags.empty?
+      where('tags && ARRAY[?]::varchar[]', tags)
+    end
   end
 end
