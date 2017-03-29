283818a7b8c855778804962454fc97280b914155
ojab <ojab@ojab.ru>
2017-03-29 18:01:02 +0000

    Inherit Sampler::Sample from ActiveRecord::Base

--- app/models/sampler/application_record.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sampler
-  class ApplicationRecord < ActiveRecord::Base # :nodoc:
-    self.abstract_class = true
-  end
-end
--- app/models/sampler/sample.rb
+++ app/models/sampler/sample.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Sampler
-  class Sample < ApplicationRecord # :nodoc:
+  class Sample < ActiveRecord::Base # :nodoc:
     validates :endpoint, :url, :request_method, presence: true
     validates :params, :request_body, :response_body, :tags,
               exclusion: { in: [nil], message: 'cannot be nil' }
